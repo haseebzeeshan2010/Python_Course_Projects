ac17c2fae419ca4294e97ba38b859746dd5bdafe
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-19 21:43:14 +0000

    Create example for implementation of args

---
+++ arg_kwarg.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    return a+b
+
+print(sum(4,5)) # Can only take 2 inputs, as there are two parameters
+
+#This is where args are used
+
+def sum_of(*args):
+    sum = 0
+    for x in args: # iterate through the arguments
+        sum += x
+    return sum
+
+print(sum_of(4,5,6,7))
