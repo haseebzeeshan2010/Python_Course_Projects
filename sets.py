666ffdb6004260dd62c2be8af1467fde4c5af22c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-19 20:56:03 +0000

    Create a sheet explaining sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+# NOTE: Sets do not take duplicate values
+set_a = {1,2,3,4,5}
+set_b = {4,5,6,7,8}
+
+print(set_a.add(6)) # add a value
+
+print(set_a.remove(6)) # removes a specified value (using discard instead of remove does the same thing)
+
+print(set_a.union(set_b)) # joins the two sets, minus the duplicate values
+
+# The same thing as union can be achieved by doing:
+#print(set_a | set_b)
