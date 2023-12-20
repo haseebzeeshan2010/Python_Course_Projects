5a8757773ef7d208af1a8401cad311b0b5076836
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-20 20:11:30 +0000

    Add examples on appending text to files

--- file_handling.py
+++ file_handling.py
@@ -15,8 +15,16 @@ with open ('file_handling_test.txt', mode = "r") as file: # Newer, better way of
     print(data)
     
 #-------------writing to files and creating files-------------------
-    
+#NOTE: Writing in python removes the file's previous contents
+
 with open ('file_handling_new_file.txt', mode = "w") as file:
     file.write("This is a new file") # writes a single line
 
-    file.writelines(["This is a new file \n", "This is another line"]) #Each string in the square brackets makes string of text, but it's actually the \n which makes a new line
+    file.writelines(["This is a new file", "\nThis is another line"]) #Each string in the square brackets makes string of text, but it's actually the \n which makes a new line
+
+#-----------------------appending to files--------------------------
+
+#NOTE: Appending in python adds to the file's previous contents
+    
+with open ('file_handling_new_file.txt', mode = "a") as file:
+    file.writelines(["This is a new file2", "\nThis is another line"])
