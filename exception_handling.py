5f8e3a862d911ac0469e2823f1fd9f44c7d55a8d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-20 19:47:33 +0000

    Add examples of error handling + class of error

--- exception_handling.py
+++ exception_handling.py
@@ -2,5 +2,12 @@ def divide_by(a,b):
     return a / b
 
 try:
+    ans = divide_by(40,0)
+except: #just outputs custome message if error occurs
+    print("Something went wrong!")
 
-except:
+try:
+    ans = divide_by(40,0)
+except Exception as e: # e is the actual error, so in this example both the custom message and the error are output
+    print("Something went wrong!", e)
+    print(e.__class__) # outputs class of error
