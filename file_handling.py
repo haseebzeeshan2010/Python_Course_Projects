224b9553a140397f4d442fd429d511b415c0d35a
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-20 20:07:45 +0000

    Make the multi-line creation example accurate

--- file_handling.py
+++ file_handling.py
@@ -19,4 +19,4 @@ with open ('file_handling_test.txt', mode = "r") as file: # Newer, better way of
 with open ('file_handling_new_file.txt', mode = "w") as file:
     file.write("This is a new file") # writes a single line
 
-    file.writelines(["This is a new file", "This is another line"]) #Each string in the square brackets makes a new line
+    file.writelines(["This is a new file \n", "This is another line"]) #Each string in the square brackets makes string of text, but it's actually the \n which makes a new line
