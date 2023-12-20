99ac0c690f5d29b49eb3b7755024b13deb2b171e
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-20 19:53:02 +0000

    Added case of handling multiple exception types

--- exception_handling.py
+++ exception_handling.py
@@ -1,18 +1,31 @@
 def divide_by(a,b):
     return a / b
 
+#Example 1
 try:
     ans = divide_by(40,0)
 except: #just outputs custome message if error occurs
     print("Something went wrong!")
 
+#Example 2
 try:
     ans = divide_by(40,0)
 except Exception as e: # e is the actual error, so in this example both the custom message and the error are output
     print("Something went wrong!", e)
     print(e.__class__) # outputs class of error
 
+#Example 3
 try:
     ans = divide_by(40,0)
 except ZeroDivisionError as e: # deals with a specific type of error
     print(e, "We cannot divide by zero")
+
+#Example 4
+try:
+    ans = divide_by(40,0)
+except ZeroDivisionError as e: # deals with a specific type of error
+    print(e, "We cannot divide by zero")
+except Exception as e: # you can add multiple exceptions for a single try clause
+    print(e, "Something went wrong")
+
+#Note: The base class for exceptions in python is Exception
