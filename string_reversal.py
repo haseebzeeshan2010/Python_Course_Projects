0dae6e97dbf626cf326fbe924f6f37ac270f2265
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-21 21:51:58 +0000

    Complete string reversal using recursion

--- string_reversal.py
+++ string_reversal.py
@@ -4,3 +4,14 @@ trial = "reversal" # The string to be reversed
 new_trial = trial[::-1]
 print(new_trial)
 
+#Using recursion
+
+def string_reverse(str):
+    if len(str) == 0:
+        return str
+    else:
+        return string_reverse(str[:1]) + str[0]
+    
+str = "reversal"
+reverse = string_reverse(str)
+print(reverse)
