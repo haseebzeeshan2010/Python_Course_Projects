0e95ae274cd272c8a37881e88faec9a1fccfd4db
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-22 21:40:39 +0000

    Create class examples and explanations for classes

---
+++ classes.py
@@ -0,0 +1,33 @@
+#---------------start of example-------------
+
+class Recipe():
+    # init(constructor) takes the objects created using the new method along with other arguments to initialize the new object being created
+
+    def __init__(self) -> None:
+        pass
+
+#----------------end of example--------------
+
+class Payslips:
+    def __init__(self, name, payment, amount) -> None:
+        self.name = name
+        self.payment = payment
+        self.amount = amount
+    
+    def pay(self):
+        self.payment = "yes"
+
+    def status(self):
+        if self.payment == "yes":
+            return self.name + " is paid" + str(self.amount)
+        else:
+            return self.name + " is not paid yet"
+
+nathan = Payslips("Nathan", "no", 1000)
+roger = Payslips("Roger", "no", 3000)
+
+print(nathan.status(), "\n", roger.status())
+
+nathan.pay()
+print("After payment")
+print(nathan.status(), "\n", roger.status())
