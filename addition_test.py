26ec112fc923857c967aa5132010869e0b18a5f4
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-12-26 19:36:14 +0000

    Test a file using pytest

--- addition_test.py
+++ addition_test.py
@@ -2,10 +2,11 @@ import addition
 import pytest
 
 def test_add():
-    assert addition.add(4, 5) == 10
+    assert True #one test
+    assert addition.add(4, 5) == 9 #another test within same function
 
 def test_sub():
-    pass
+    pass # passes this test
 
 
 #to test, write:  python3 -m pytest addition_test.py
